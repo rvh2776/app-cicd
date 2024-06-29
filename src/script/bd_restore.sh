bf4838be166284ab0ba8fa8e303a0a16d8208c69
Rafael V.H <rafael.vh@gmail.com>
2024-06-29 15:53:13 -0300

    Cambio directorios

--- src/script/bd_restore.sh
+++ src/script/bd_restore.sh
@@ -3,18 +3,21 @@
 # Variables
 CONTAINER_NAME="app-cicd_postgres-db_1"
 DB_USER="rafael"
-BACKUP_FILE="backup.sql"
+DB_TO_CONNECT="postgres"
+BACKUP_FILE="/usr/src/app-cicd/src/script/backup.sql"
 
 # Extraer los nombres de las bases de datos del archivo de respaldo
 DB_NAMES=$(grep -oP '(?<=CREATE DATABASE )\w+' $BACKUP_FILE)
 
 # Crear cada base de datos si no existe
+
 for DB_NAME in $DB_NAMES; do
-  echo "Creando base de datos: $DB_NAME"
-  docker exec -i $CONTAINER_NAME psql -U $DB_USER -c "CREATE DATABASE $DB_NAME;"
+ echo "Creando base de datos: $DB_NAME"
+ docker exec -i $CONTAINER_NAME psql -U $DB_USER -d $DB_TO_CONNECT -c "CREATE DATABASE $DB_NAME;" 2>/dev/null
 done
 
 # Restaurar el respaldo
-cat $BACKUP_FILE | docker exec -i $CONTAINER_NAME psql -U $DB_USER
 
-echo "Restauración de las bases de datos completada."
+cat $BACKUP_FILE | docker exec -i $CONTAINER_NAME psql -U $DB_USER -d $DB_TO_CONNECT
+
+echo "Restauración de las bases de datos completada.
